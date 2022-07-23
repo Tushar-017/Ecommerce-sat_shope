ef7894a3ad9ed3ed0ee807abd09f52a11e6919a8
Tushar Rajput <tusharo17rajput@gmail.com>
2022-07-23 19:09:18 +0530

    homepage, directory & menuitem added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Homepage from './pages/homepage/Homepage';
 import './App.css';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+            <Route  path='/' element={<Homepage/>}/>
+            <Route path='/hats' element={<HatsPage/>}/>
+      </Routes>
   );
 }
 
