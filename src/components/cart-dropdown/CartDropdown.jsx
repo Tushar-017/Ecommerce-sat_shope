5ecc80c014082872804b1ea2bc25e884f4d88969
Tushar Rajput <tusharo17rajput@gmail.com>
2022-08-01 16:59:01 +0530

    adding add to cart feture

--- src/components/cart-dropdown/CartDropdown.jsx
+++ src/components/cart-dropdown/CartDropdown.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, {useContext} from 'react'
+
+import { CartContext } from '../../contexts/cart.context'
 
 import CustomButton from '../custom-button/CustomButton'
+import CartItem from '../cart-item/CartItem'
 
 import './CartDropdown.scss'
 
 const CartDropdown = () => {
-  return (
-    <div className='cart-dropdown-container'>
-      <div className='cart-items'/>
-      <CustomButton>GO TO CHECKOUT</CustomButton>
-    </div>
-  )
+   const {cartItems} = useContext(CartContext) 
+
+   return (
+      <div className='cart-dropdown-container'>
+            <div className='cart-items'>
+               {cartItems.map(item => (
+                  <CartItem key={item.id} cartItem={item} />
+               ))}
+            </div>
+         <CustomButton>GO TO CHECKOUT</CustomButton>
+      </div>
+   )
 }
 
 export default CartDropdown
--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
+
+import { CartContext } from '../../contexts/cart.context'
+
 import CustomButton from '../custom-button/CustomButton'
 
 import './CollectionItem.scss'
 
 const CollectionItem = ({product}) => {
   const {name,imageUrl,price} = product;
+  const { addItemToCart} = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="collection-item">
       <img src={imageUrl} alt={name} />
@@ -12,7 +19,7 @@ const CollectionItem = ({product}) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton buttonType='inverted'>Add to cart</CustomButton>
+      <CustomButton buttonType='inverted' onClick={addProductToCart}>Add to cart</CustomButton>
     </div>
   )
 }
